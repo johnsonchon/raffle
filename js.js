03622947e1e0a7bf3eee3e5893ca7d9a915fdc6f
johnsonchon <39934039+johnsonchon@users.noreply.github.com>
2019-02-13 14:29:47 -0800

    Add files via upload

--- js.js
+++ js.js
@@ -2,28 +2,12 @@ var card = $(".card-img-top");
 var cardB = $(".card");
 
 card.on("click", function () {
-    if ("opacity", "1") {
-        $(this).css({
-            "opacity": "0.2",
-        });
-
-    }
-
-    else if ("opacity", "0.2") {
-        $(this).css({
-            "opacity": "1",
-        });
-    }
+    $(this).toggleClass("card-click");
+    
 });
 
 
-cardB.on("click",function () {
-    if ("opacity", "1") {
-        $(this).css({
-            "background-color": "#C0C0C0",
-            "border": "none"
+cardB.on("click", function () {
+    $(this).toggleClass("card-b-click");
 
-        });
-
-    }
 });
